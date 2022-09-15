c801e52ae545e225b1bf6fb5f98dfefed07fd414
PVZMF <mhmoghadamfar@gmail.com>
2022-09-15 04:50:37 +0430

    Add Searchbar js and some change on color table

--- src/App.js
+++ src/App.js
@@ -16,6 +16,18 @@ function updateTable(num){
         let tbody = document.querySelector("#tbody");
         let tr = tbody.children[num-1];
         let date = tr.children[0].innerHTML;
+
+        let alltask = dbTodos.filter(elem => elem.date === date).length;
+        let Donetask = dbTodos.filter(elem => elem.date === date && elem.status).length;
+        let NotDonetask = dbTodos.filter(elem => elem.date === date && !elem.status).length;
+        if(alltask === Donetask){
+            tr.classList.add("bg-green-100","border-green-200");
+        }
+        else if(alltask-Donetask>Donetask){
+            tr.classList.add("bg-red-100","border-red-200");
+        }else if(alltask-Donetask<Donetask){
+            tr.classList.add("bg-blue-100","border-blue-200");
+        }
         tr.children[1].innerHTML= dbTodos.filter(elem => elem.date === date).length;
         tr.children[2].innerHTML= dbTodos.filter(elem => elem.date === date && elem.status).length;
         tr.children[3].innerHTML= dbTodos.filter(elem => elem.date === date && !elem.status).length;
@@ -25,23 +37,31 @@ function updateTable(num){
         let tbody = document.querySelector("#tbody");
         tbody.innerHTML =[];
         uniqueDate.forEach((date,i) => {
-        let alltask = dbTodos.filter(elem => elem.date === date);
-        let Donetask = dbTodos.filter(elem => elem.date === date && elem.status);
-        let NotDonetask = dbTodos.filter(elem => elem.date === date && !elem.status);
+        let alltask = dbTodos.filter(elem => elem.date === date).length;
+        let Donetask = dbTodos.filter(elem => elem.date === date && elem.status).length;
+        let NotDonetask = dbTodos.filter(elem => elem.date === date && !elem.status).length;
         let tr = document.createElement("tr");
         tr.classList.add("border-b");
+        if(alltask === Donetask){
+            tr.classList.add("bg-green-100","border-green-200");
+        }
+        else if(alltask-Donetask>Donetask){
+            tr.classList.add("bg-red-100","border-red-200");
+        }else if(alltask-Donetask<Donetask){
+            tr.classList.add("bg-blue-100","border-blue-200");
+        }
         let tdtime = document.createElement("td");
         tdtime.classList.add("text-sm","text-gray-900","font-medium","px-6","py-4","whitespace-nowrap");
         tdtime.innerHTML = date;
         let tdAll = document.createElement("td");
         tdAll.classList.add("text-sm","text-gray-900","font-medium","px-6","py-4","whitespace-nowrap");
-        tdAll.innerHTML = alltask.length;
+        tdAll.innerHTML = alltask;
         let tdDone = document.createElement("td");
         tdDone.classList.add("text-sm","text-gray-900","font-medium","px-6","py-4","whitespace-nowrap");
-        tdDone.innerHTML = Donetask.length;
+        tdDone.innerHTML = Donetask;
         let tdNotDone = document.createElement("td");
         tdNotDone.classList.add("text-sm","text-gray-900","font-medium","px-6","py-4","whitespace-nowrap");
-        tdNotDone.innerHTML = NotDonetask.length;
+        tdNotDone.innerHTML = NotDonetask;
         let button = document.createElement("td");
         button.classList.add("inline-block","px-6","py-2.5","bg-green-600","text-white","font-medium","text-xs","leading-tight","uppercase","rounded","shadow-md","hover:bg-blue-700","hover:shadow-lg","focus:bg-blue-700","focus:shadow-lg","focus:outline-none","focus:ring-0","active:bg-blue-800","active:shadow-lg","transition","duration-150","ease-in-out");
         button.innerHTML = "Update";
@@ -93,11 +113,20 @@ function NotDoneTasks(){
     let dbDone = dbTodos.filter(elem => !elem.status);
     render(dbDone);
 }
-
+let keyup = "";
+function Search(e){
+    if(e.key ==="Backspace"){
+        keyup = keyup.slice(0, keyup.length - 1)
+    }else{
+        keyup += e.key;
+    }
+    let finddbTodos = dbTodos.filter(elem => elem.data.includes(keyup));
+    render(finddbTodos);
+}
 let dbTodos = [];
 
-document.querySelector("#btnAdd").addEventListener('click',()=>Add())
-
+document.querySelector("#btnAdd").addEventListener('click',()=>Add());
+document.querySelector("#search").addEventListener("keyup",(e)=> Search(e));
 function render(dbTodos){
     let listtodos = document.querySelector("#listTodos");
     listtodos.innerHTML=[];
@@ -124,7 +153,9 @@ function render(dbTodos){
         }
         p.innerHTML = element.data;
         btnDone.addEventListener('click',() => NotDone(i));
+        btnDone.addEventListener('click',() => DoneTasks());
         btnNotDone.addEventListener('click',() => Done(i));
+        btnNotDone.addEventListener('click',() => NotDoneTasks());
         btnRemove.addEventListener('click',() => Remove(i));
         listtodos.append(newtodo);
         newtodo.append(p,btnDone,btnNotDone,btnRemove)
